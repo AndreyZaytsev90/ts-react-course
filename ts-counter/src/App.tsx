871529484be6f8d0d23732d6aac346ea5fc2fa39
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2026-01-15 21:48:53 +0300

    update

--- ts-counter/src/App.tsx
+++ ts-counter/src/App.tsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Header(){
+  return (
+    <h1>Vite + React</h1>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
