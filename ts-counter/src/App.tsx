82f3194c4e4464843e9b96602290a8d6b2fafcb6
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2026-01-15 22:25:09 +0300

    update settings

--- ts-counter/src/App.tsx
+++ ts-counter/src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function Header(){
-  return (
-    <h1>Vite + React</h1>
-  )
+function Header() {
+  return <h1>Vite + React</h1>;
 }
+
+const Header2 = () => {
+  return <h1>Vite + React</h1>;
+};
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header2 />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
