8150fc5a532d31ee6d82f7b436a0c2fc9a5f2d5d
Zaycev <weddeveloper2020@gmail.com>
2026-01-16 13:17:36 +0300

    add new component

--- ts-counter/src/App.tsx
+++ ts-counter/src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
 import './App.css';
+import { Logos } from './Logos';
+import { Counter } from './Counter';
 
 function Header() {
     return <h1>Vite + React</h1>;
@@ -13,28 +14,12 @@ const Header2 = ({ firstName }: Header2props) => {
 };
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <Header2 firstName="Andrey" />
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src="/react.svg" className="logo react" alt="React logo" />
-                </a>
-            </div>
+            <Logos />
             <Header />
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-            <div className="read-the-docs">Привет!</div>
+            <Counter />
         </>
     );
 }
---
+++ ts-counter/src/Logos.tsx
@@ -0,0 +1,12 @@
+export function Logos() {
+    return (
+        <div>
+            <a href="https://vite.dev" target="_blank">
+                <img src="/vite.svg" className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+                <img src="/react.svg" className="logo react" alt="React logo" />
+            </a>
+        </div>
+    );
+}
